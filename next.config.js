7bc6b9c6d3c18d824c0127fe8259bac5c6744ea4
aleks <aleksandr.kozlyakov@yandex.ru>
2022-04-24 22:45:38 +0300

    added eslint and typeScript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['./src/assets/styles'],
+    prependData: `@import "variables.scss";`
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(ts)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
