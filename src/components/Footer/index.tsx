6e6a487a1cc2073c5d2d3947a3dace8ef9b20a38
aleks <aleksandr.kozlyakov@yandex.ru>
2022-10-19 21:23:44 +0300

    RA-295: changed url for img

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,10 +8,6 @@ export function Footer() {
         <BaseLink href="/" className={styles.footerLogo}>
           <img src="/images/logo.svg" alt="" />
         </BaseLink>
-        {/*
-        TODO Футер меню переделать -
-        TODO оно держится на костылях только при условии того, что пункты меню там статичны
-        */}
         <FooterMenu className={styles.footerMenu} />
         <BaseButton size="middle" color="accent" className={styles.footerButton}>
           Raise a complaint
