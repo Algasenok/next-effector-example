b72be50b33a695267c81252bdc08c65fa10a0c07
aleks <aleksandr.kozlyakov@yandex.ru>
2022-10-20 13:43:32 +0300

    RA-295: fixed url

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
 export const CMS_URL =
-  process.env.NODE_ENV === 'development' ? 'https://cms.lottery.stage.plat.agency' : 'API_CRM_URL';
+  process.env.NODE_ENV === 'development'
+    ? 'https://cms.lottery.stage.plat.agency'
+    : 'https://cms.lottery.stage.plat.agency';
 
 export const SPA_URL =
   process.env.NODE_ENV === 'development' ? 'https://spa.lottery.stage.plat.agency' : 'SPA_BASE_URL';
