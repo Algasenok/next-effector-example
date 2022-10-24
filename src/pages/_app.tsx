9ac6494cb8d4becd6fdef2736c3579d3b389a6c2
aleks <aleksandr.kozlyakov@yandex.ru>
2022-10-24 16:56:53 +0300

    fixed app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,5 @@
 import '@/styles/globals.scss';
-import '@/styles/singlePageStyles.scss';
-import Head from 'next/head';
-import type { AppProps } from 'next/app';
-import * as effectorReact from 'effector-react/scope';
+import App from 'next/app';
 import { withEffector } from 'nextjs-effector';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
-
-export default withEffector(MyApp, { effectorReact });
+export default withEffector(App);
